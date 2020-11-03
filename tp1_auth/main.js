9b8918336c710f3d3aabd93f065773c2d801e49b
charlottedrb <c.derbaghdassarian@gmail.com>
2020-11-03 21:33:46 +0100

    ajout des traits

--- tp1_auth/main.js
+++ tp1_auth/main.js
@@ -4,21 +4,40 @@ document.addEventListener("DOMContentLoaded", () => {
         e.preventDefault()
         document.querySelector("#bg").classList.remove("hidden")
 
+        const canvas = document.querySelector("#auth canvas");
+        const c = canvas.getContext("2d")
+        let ox, oy, x, y
+
         const circles = document.querySelectorAll(".circle")
         const auth = document.querySelector("#auth")
+
         let code = ""
 
         circles.forEach((circle, i) => {
             circle.addEventListener("mouseenter", () => {
                 if(code.indexOf(i) == -1){
                     code += i;
+                    circle.classList.add("selected")
+    
+                    //Rectangle englobant le circle
+                    let rect = circle.getBoundingClientRect()
+    
+                    if(code.length == 1){
+                        //Le code n'a qu'un seul caractère, premier point
+                        c.beginPath()
+                        c.moveTo(circle.offsetLeft + rect.width / 2, circle.offsetTop + rect.height / 2)
+                    } else {
+                        //les autres points 
+                        c.lineTo(circle.offsetLeft + rect.width / 2, circle.offsetTop + rect.height / 2)
+                        c.strokeStyle = "lightpink"
+                        c.lineWidth = 3
+                        c.stroke()
+                    }
                 }
-                circle.classList.add("selected")
             })
         });
 
         auth.addEventListener("mouseleave", () => {
-            
             fetch("auth.php?code=" + code)
             .then((response) => {
                 return response.json()
@@ -37,6 +56,7 @@ document.addEventListener("DOMContentLoaded", () => {
                         selectedCircle.classList.remove("selected-ok")
                         selectedCircle.classList.remove("selected-fail")
                     });
+                    c.clearRect(0, 0, canvas.width, canvas.clientHeight)
                     if(data.ok){
                         window.location = "admin"
                     }
