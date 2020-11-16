71d85df4d84d6d9ceb2f86b68addd46823ff386c
charlottedrb <c.derbaghdassarian@gmail.com>
2020-11-16 16:42:33 +0100

    dessin collaboratif

--- tp3_socket/canvas/main.js
+++ tp3_socket/canvas/main.js
@@ -1,17 +1,40 @@
-const canvas = document.querySelector("canvas")
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
-const c = canvas.getContext("2d")
-const w = canvas.width
-const h = canvas.height
+document.addEventListener("DOMContentLoaded", () => {
+    //io appelle l'api
+    const socket = io("51.83.36.122:8080")
+    const canvas = document.querySelector("canvas")
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+    const c = canvas.getContext("2d")
+    const w = canvas.width
+    const h = canvas.height
 
-let p = { x:null, y: null}
+    canvas.addEventListener("mousemove", (e) => {
+        //quand on appuie pas sur le bouton e.buttons = 0
+        if(e.buttons != 0){
+            socket.emit("point", 
+            {
+                x: e.pageX, 
+                y: e.pageY, 
+                color: "lavender"
+            })
+        }
+    })
+    socket.on("draw", (enable) => {
+        if(enable){
+            c.beginPath()
+            c.rect(0, 0, w, 10)
+            c.fillStyle = "lavender"
+            c.fill()
+        } else {
+            c.clearRect(0, 0, w, 10)
+        }
+    })
 
-canvas.addEventListener("mousemove", (e) => {
-    c.beginPath()
-    c.lineWidth = 4
-    c.strokeStyle = "pink"
-    c.arc(e.pageX, e.pageY, 10, 0, Math.PI*2)
-    c.fillStyle = linear-gradient(orange, yellow, green, cyan, blue, violet)
-    c.fill()
+    socket.on("point", (data) => 
+    {
+        c.beginPath()
+        c.arc(data.x, data.y, 5, 0, Math.PI*2)
+        c.fillStyle = data.color
+        c.fill()
+    })
 })
